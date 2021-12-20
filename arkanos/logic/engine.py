8e5cd6fe09e466bd4c600858c4538e64041c14cb
Konrad Gawda <k.gawda@alx.pl>
2021-12-20 12:33:30 +0100

    Add game engine and type hints

---
+++ arkanos/logic/engine.py
@@ -0,0 +1,17 @@
+import random
+from typing import List
+
+from logic.pieces import Piece
+
+
+class Engine:
+    def __init__(self, width, height):
+        self.width: int = width
+        self.height: int = height
+        self.pieces: List[Piece] = []
+
+    def add_piece_in_random_place(self, piece: Piece):
+        piece.x = random.randrange(self.width)
+        piece.y = random.randrange(self.height)
+        self.pieces.append(piece)
+
--- arkanos/main.py
+++ arkanos/main.py
@@ -1,9 +1,13 @@
 from logic.pieces import Piece
+from logic.engine import Engine
 
 
 def main():
+    engine = Engine(80, 20)
     player1 = Piece("Arkanos", 0, 0)
-    print(player1.name)
+    engine.add_piece_in_random_place(player1)
+    for p in engine.pieces:
+        print(f"  {p.name} ({p.x}, {p.y})")
 
 
 if __name__ == '__main__':
