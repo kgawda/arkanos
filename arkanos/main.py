219972e29f6472030bbef7c835af8a12d9d2468f
Konrad Gawda <k.gawda@alx.pl>
2021-12-21 09:39:42 +0100

    Implement ConsolePrinter

--- arkanos/main.py
+++ arkanos/main.py
@@ -1,5 +1,6 @@
 from logic.pieces import Piece
 from logic.engine import Engine
+from ui.console import ConsolePrinter
 
 
 def main() -> None:
@@ -7,12 +8,14 @@ def main() -> None:
     # - annotacja typu zwracanego, lub
     # - mypy arkanos --check-untyped-defs
     engine = Engine(80, 20)
+    printer = ConsolePrinter(engine)
     player1 = Piece("Arkanos", 0, 0)
     # player2 = Piece("Barbarios", 0, 0)
     engine.add_piece_in_random_place(player1)
     for p in engine.pieces:
         print(f"  {p.name} ({p.x}, {p.y})")
 
+    printer.print()
 
 if __name__ == '__main__':
     main()
--- arkanos/ui/console.py
+++ arkanos/ui/console.py
@@ -1,6 +1,14 @@
 class ConsolePrinter:
     def __init__(self, game_engine):
-        ...
+        self.engine = game_engine
 
     def print(self):
-        ...
+        for y in range(self.engine.height - 1, -1, -1):
+            for x in range(self.engine.width):
+                pieces = list(self.engine.pieces_at(x, y))
+                if pieces:
+                    character = pieces[0].one_character()
+                else:
+                    character = '.'
+                print(character, end='')
+            print()
