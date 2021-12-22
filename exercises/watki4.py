43794278f79c710172e2188b18032f4694b0262f
Konrad Gawda <k.gawda@alx.pl>
2021-12-22 15:30:08 +0100

    Dokończenie wątków

--- exercises/watki4.py
+++ exercises/watki4.py
@@ -3,9 +3,10 @@ import time
 
 def f(lock):
     while True:
-        with lock:
+        with lock:  # to nie jest dobry pomysł żeby długo przebywać "pod lockiem"
             print("Działa wątek", threading.get_ident(), '*' * (threading.get_ident() % 16))
             time.sleep(1)
+        time.sleep(0.000001)  # <- potrzebne żeby płynnie przełączało się między wątkami (w szczególności na Linux)
 
 if __name__ == '__main__':
     lock = threading.Lock()
---
+++ exercises/watki5.py
@@ -0,0 +1,22 @@
+import urllib.request
+import concurrent.futures
+import time
+
+def f(url):
+    t1 = time.perf_counter()
+    urllib.request.urlopen(url).read()
+    t2 = time.perf_counter()
+    return f"Czas {t2-t1} s dla {url}"
+
+dane = [
+    'https://wp.pl',
+    'https://onet.pl',
+    'https://google.com',
+    'https://alx.pl',
+]
+
+if __name__ == '__main__':
+    with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
+        for wynik in executor.map(f, dane):
+            print(wynik)
+
